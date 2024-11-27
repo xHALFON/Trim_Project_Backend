444c053df4ec071442b680e146ef46ef74a7a548
TuvalYulevich <Tuval.Yulevich@gmail.com>
2024-11-27 17:59:35 +0200

    Didn't save before. Sorry

---
+++ models/userModel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', userSchema);
