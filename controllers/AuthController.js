e1b16fa98c99be4386148cc7521e719a6a7df6fc
xHALFON <guyhalfon98@gmail.com>
2024-11-27 18:09:04 +0200

    added login and register controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,52 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+// jwt
+const generateToken = (id) => {
+    return jwt.sign({ id }, "1234", { expiresIn: '1h' });
+};
+
+export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+
+        res.status(201).json({
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !(bcrypt.compare(user.password, password))) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        res.json({
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
