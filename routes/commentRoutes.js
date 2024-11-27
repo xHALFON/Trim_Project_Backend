352a302a50d9b90cf225bfe6584fd7702ad82237
TuvalYulevich <Tuval.Yulevich@gmail.com>
2024-11-27 15:50:55 +0200

    Fixed something

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
     createComment,
-    getAllComments
+    getAllComments,
+    getCommentsByPost,
+    getCommentsByUser
 } from "../controllers/commentController.js";
 
 const router = express.Router();
